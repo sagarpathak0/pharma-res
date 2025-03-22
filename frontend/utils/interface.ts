f2dce2fbeae2d1eb15f612f29363190462816942
Anupam <9582anupamk@gmail.com>
2025-03-22 21:22:37 +0530

    added interface

---
+++ frontend/utils/interface.ts
@@ -0,0 +1,21 @@
+interface Student {
+    name: string;
+    roll: number;
+    campus: string;
+    program: string;
+    result: Result[];
+}
+interface Result {
+    year: number;
+    marks?: Subject[];
+    // reappear?: Subject[];
+}
+
+interface Subject {
+    course_code: string;
+    course_name: string;
+    marks_obtained: string;
+    month_year: string;
+}
+
+export type { Student, Result, Subject };
